3577da32c033ec75b2f8fb36be52f9dddc7ec999
Jacob Akhmedov <56736396+Jacobamv@users.noreply.github.com>
2023-03-28 06:22:04 +0000

    123

--- src/components/templates/TeamDisplay/index.tsx
+++ src/components/templates/TeamDisplay/index.tsx
@@ -6,17 +6,20 @@ function TeamDisplay() {
   return (
     <div className='templates__team-display'>
       <Sticky isNav={true}>MadDevs</Sticky>
+      <p>In this part, you have the opportunity to see detailed information about each of the directors <br/> by clicking on the photo</p>
       <Icons />
       <Text>
         <div>
           <div>
-            By clicking on the blocks below you will be given the opportunity to
+            Also, by clicking on the blocks below you will be given the opportunity to
             view in detail information about each aspect of our work.
           </div>
           <div>Enjoy!</div>
         </div>
       </Text>
       <PersonalLink />
+      <br/>
+      <br/>
     </div>
   );
 }
