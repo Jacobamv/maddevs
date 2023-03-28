fae015ef4dd83564cafa22a1c5f06e43cd996043
beastkasper <62433555+beastkasper@users.noreply.github.com>
2023-03-28 07:40:26 +0500

    .

--- src/components/modules/intro/index.tsx
+++ src/components/modules/intro/index.tsx
@@ -1,15 +1,27 @@
 import "./intro.scss";
 import FirstIcon from "./circles/first";
-
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
 export default function Intro() {
   return (
-    <div className="container">
-    <div className="Circle">
-        <FirstIcon/>
-    </div>
+    <div className="container2">
+      <div className="wrap">
+        
+        <div className="Circle">
+            <FirstIcon/>
+        </div>
+        <h1 className="title" >Welcome to MD</h1>
+        <p className='desc' >Our team will present you an interactive solution to The Digital Divide</p>
 
-    <h1>Welcome to MD</h1>
+        <div className="dotsWrap">
+          <div className="dot active"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+        </div>
+        <button type="submit" className="btn" >Let’s start!</button>
+      </div>
     </div>
   );
 }
